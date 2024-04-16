49eb27caadb7e0dab981465464019962fa3634a9
Mlie <emipa606@users.noreply.github.com>
2024-04-16 22:15:39 +0200

    Fixed the power-output issue

--- Source/MechPowerSettings/MechPowerMod.cs
+++ Source/MechPowerSettings/MechPowerMod.cs
@@ -4,14 +4,18 @@ namespace MechPowerSettings;
 
 internal class MechPowerMod : ModSettings
 {
-    internal static float poweroutput = 5000f;
+    internal static float poweroutput = -5000f;
 
     internal static float marketvalue = 2000f;
 
     public override void ExposeData()
     {
         base.ExposeData();
-        Scribe_Values.Look(ref poweroutput, "powerOutput", 5000f);
+        Scribe_Values.Look(ref poweroutput, "powerOutput", -5000f);
         Scribe_Values.Look(ref marketvalue, "marketValue", 2000f);
+        if (poweroutput > 0f)
+        {
+            poweroutput = -poweroutput;
+        }
     }
 }
